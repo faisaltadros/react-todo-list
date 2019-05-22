2c6db7ada7dd1371f04f99bc034cb67649fcb82b
Faisal <faisal@rotatestudios.com>
2019-05-22 15:12:42 -0500

    Updates

---
+++ precache-manifest.b27024cec93027fdc05cb831ef05d092.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b996e234dd1d921b4f717f4b2b10ef19",
+    "url": "/react-todo-list/index.html"
+  },
+  {
+    "revision": "7bae4c2c7a8c8855e19c",
+    "url": "/react-todo-list/static/css/main.3264cb43.chunk.css"
+  },
+  {
+    "revision": "7f3ea3297552452fa97b",
+    "url": "/react-todo-list/static/js/2.f2039c18.chunk.js"
+  },
+  {
+    "revision": "7bae4c2c7a8c8855e19c",
+    "url": "/react-todo-list/static/js/main.9aa55b3b.chunk.js"
+  },
+  {
+    "revision": "89737d6b5129f89a8a0b",
+    "url": "/react-todo-list/static/js/runtime~main.eb2f80b6.js"
+  }
+]);
