b2f6350a16262072f61486679cb9192c8268def6
Faisal <faisal@rotatestudios.com>
2019-05-22 16:49:40 -0500

    route fix

--- src/App.js
+++ src/App.js
@@ -64,14 +64,14 @@ class App extends React.Component {
         <div className="App" >
           <div className="container">
             <Header />
-            <Route exact path="/" render={props => (
+            <Route exact path="/react-todo-list/" render={props => (
               <React.Fragment>
                 <AddTodo addTodo={this.addTodo} />
                 <Todos todos={this.state.todos} markComplete={this.markComplete}
                   delTodo={this.delTodo} />
               </React.Fragment>
             )} />
-            <Route path="/about" component={About} />
+            <Route path="/react-todo-list/about" component={About} />
           </div>
         </div>
       </Router>
